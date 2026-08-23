0f326b34d910dc2e0ada1494f0eb28d1150cf466
muhammadsameerdeveloper <muhammadsameerdeveloper@gmail.com>
2026-08-23 20:59:05 +0500

    file setup

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static Color whiteColor = const Color(0xffFFFFFF);
+  static Color orangiColor = const Color(0xffFFA451);
+  static Color greyColor = const Color(0xffF3F4F9);
+  static Color textGreyColor = const Color(0xff86869E);
+  static Color blackColor = const Color(0xff27214D);
+  static Color lightPinkColor = const Color(0xffFFF2E7);
+  static Color shadowColor = const Color(0xffE5E5E5);
+  static Color lightOrangiColor = const Color(0xffFFFAEB);
+  static Color lightPink = const Color(0xffFEF0F0);
+  static Color lightGreyColor = const Color(0xffF1EFF6);
+}
